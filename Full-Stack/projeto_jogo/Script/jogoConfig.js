8ba7e471a142294c0f0a71dfa6eaee7905a58174
Jessybr <jessicabuenoramos5@gmail.com>
2024-03-23 19:31:04 -0300

    :sparkles:

--- Full-Stack/projeto_jogo/Script/jogoConfig.js
+++ Full-Stack/projeto_jogo/Script/jogoConfig.js
@@ -7,25 +7,28 @@ if(pontos <= 0){
     const h2_pontos=document.createElement("h2")
     h2_pontos.innerHTML="VOCÊ PERDEU!"
     cont_pontos.appendChild(h2_pontos)
-
-    cont_pontos.setAttribute("style", "width: 250px; color: white; text-align: center; border-radius: 15px; position: absolute; top: 30%; left: 42%; background-color: #FFC857;")
+    cont_pontos.classList.add("cont_pontos")
 
     const div_start=document.createElement("div")
     div_start.setAttribute("class", "start")
+
     const form_start=document.createElement("form")
     form_start.setAttribute("method", "POST")
     form_start.setAttribute("action", "index.php")
     div_start.appendChild(form_start)
+
     const input1_start=document.createElement("input")
     input1_start.setAttribute("id", "voltar")
     input1_start.setAttribute("type", "hidden")
     input1_start.setAttribute("name", "acao")
     input1_start.setAttribute("value", "Voltar")
     form_start.appendChild(input1_start)
+
     const input2_start=document.createElement("input")
     input2_start.setAttribute("id", "voltar")
     input2_start.setAttribute("type", "submit")
     input2_start.setAttribute("value", "Voltar")
+    input2_start.classList.add("voltar-inp")
     form_start.appendChild(input2_start)
     cont_pontos.appendChild(div_start)
 
